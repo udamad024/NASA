373f2adbfe1f0e17f7a9cd026160e2c571de6119
ashen0101 <udamad123@gmail.com>
2024-04-18 11:13:33 -0400

    changes to add images to webserver

--- webservers/install_httpd.sh
+++ webservers/install_httpd.sh
@@ -2,6 +2,6 @@
 sudo yum -y update
 sudo yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1> My private IP is $myip</h1><br>Built by Terraform!"  >  /var/www/html/index.html
+echo "<html><body><img src='https://mykey101.s3.amazonaws.com/new.jpg'></body></html>" > /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
