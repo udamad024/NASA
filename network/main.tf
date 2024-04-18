e2ca90c83b787567e04b0dbf648d738c59ec1b51
ashen0101 <udamad123@gmail.com>
2024-04-18 14:21:57 -0400

    sada

--- network/main.tf
+++ network/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-#ashgi
+#ashgiasd
 # Data source for availability zones in us-east-1
 data "aws_availability_zones" "available" {
   state = "available"
